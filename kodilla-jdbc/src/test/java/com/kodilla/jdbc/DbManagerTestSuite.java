4074e05a0d43b092183da3cacfd9b834e9fcc187
Hamonnah <hamonnah@Hamonnahs-MacBook-Air.local>
2020-09-17 14:42:18 +0200

    testSelectUsersAndPosts

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -22,12 +22,10 @@ public class DbManagerTestSuite {
     public void testSelectUsers() throws SQLException {
         //Given
         DbManager dbManager = DbManager.getInstance();
-
         //When
         String sqlQuery = "SELECT * FROM USERS";
         Statement statement = dbManager.getConnection().createStatement();
         ResultSet rs = statement.executeQuery(sqlQuery);
-
         //Then
         int counter = 0;
         while(rs.next()) {
@@ -40,5 +38,24 @@ public class DbManagerTestSuite {
         statement.close();
         Assert.assertEquals(21, counter);
     }
+
+    @Test
+    public void testSelectUsersAndPosts() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        //When
+        String sqlQuery = "SELECT U.FIRSTNAME, U.LASTNAME, COUNT(*) FROM USERS U JOIN POSTS P ON U.ID = p.USER_ID GROUP BY U.ID HAVING COUNT(*) >= 2;";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        Assert.assertEquals(3, counter);
+    }
+
 
 }
