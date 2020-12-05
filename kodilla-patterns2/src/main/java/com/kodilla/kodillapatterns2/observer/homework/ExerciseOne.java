5b58e2d6c439d5a1d40212722eae468f9568512f
Hamonnah <hamonnah@Hamonnahs-MacBook-Air.local>
2020-12-05 22:11:39 +0100

    another requested changeses done

--- kodilla-patterns2/src/main/java/com/kodilla/kodillapatterns2/observer/homework/ExerciseOne.java
+++
@@ -1,9 +0,0 @@
-package com.kodilla.kodillapatterns2.observer.homework;
-
-public class ExerciseOne extends ExercisesQueue {
-
-    public ExerciseOne() {
-        super("Hello World guide");
-    }
-
-}
--- kodilla-patterns2/src/main/java/com/kodilla/kodillapatterns2/observer/homework/ExerciseTwo.java
+++
@@ -1,9 +0,0 @@
-package com.kodilla.kodillapatterns2.observer.homework;
-
-public class ExerciseTwo extends ExercisesQueue {
-
-    public ExerciseTwo() {
-        super("Learn Git and GitHub");
-    }
-
-}
