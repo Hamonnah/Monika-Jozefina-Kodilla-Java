a90ebb2d00f1a64ac9d802401f7b869bfa269b68
Hamonnah <hamonnah@Hamonnahs-MacBook-Air.local>
2021-01-06 06:35:12 +0100

    ex31.1

--- kodilla-testing2/src/main/java/ebay/EbayTestingApp.java
+++ kodilla-testing2/src/main/java/ebay/EbayTestingApp.java
@@ -1,2 +1,22 @@
-package ebay;public class EbayTestingApp {
+package ebay;
+
+import config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class EbayTestingApp {
+
+    public static final String SEARCHFIELD = "_nkw";
+
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.ebay.com/");
+
+        WebElement searchField = driver.findElement(By.name(SEARCHFIELD));
+        searchField.sendKeys("Laptop");
+        searchField.submit();
+    }
+
 }
