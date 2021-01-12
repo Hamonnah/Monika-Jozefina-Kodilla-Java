ebd58faa8318c5b9663f9c7eca96ad2c2c222396
Hamonnah <hamonnah@Hamonnahs-MacBook-Air.local>
2021-01-12 20:49:16 +0100

    selenium

--- kodilla-testing2/src/main/java/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/facebook/FacebookTestingApp.java
@@ -6,18 +6,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.concurrent.TimeUnit;
+
 public class FacebookTestingApp {
 
     public static final String XPATH_ACCEPT_ALL_BUTTON = "//div[contains(@class, \"_9fiw\")]/button[2]";
     public static final String XPATH_REGISTRATION_FORM_BUTTON = "//div[contains(@class, \"6ltg\")]/a[1]";
     public static final String XPATH_REGISTRATION_BOX = "//*[@id=\"reg_box\"]/div[1]";
-    public static final String XPATH_SELECT_DAY_COMBOBOX = "//div[contains(@class, \"_5k_5\")]/span/select";
+    public static final String XPATH_SELECT_DAY_COMBOBOX = "//select[@id=\"day\"]";
     public static final String XPATH_SELECT_MONTH_COMBOBOX = "//div[contains(@class, \"_5k_5\")]/span/select[2]";
     public static final String XPATH_SELECT_YEAR_COMBOBOX = "//div[contains(@class, \"_5k_5\")]/span/select[3]";
 
     public static void main(String[] args) {
 
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("https://www.facebook.com/");
 
         WebElement acceptAll = driver.findElement(By.xpath(XPATH_ACCEPT_ALL_BUTTON));
